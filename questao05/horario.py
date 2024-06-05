f3a7dbd5b9a8a7a89ccd7c2df45417d9c2c45ca7
natanferreirah <167040989+natanferreirah@users.noreply.github.com>
2024-06-05 09:56:07 -0300

    Atividade

--- questao05/horario.py
+++ questao05/horario.py
@@ -1,11 +1,11 @@
-hora = int(input("digite um horário: "))
+hora = int(input("Digite um horário: "))
 if hora < 0 or hora > 23:
-    print("digite um número entre 0 e 23")
+    print("Digite um número entre 0 e 23")
 else:
     if  4 < hora < 12:
-        print("está de manhã")
+        print("Está de manhã")
     elif 11 < hora < 18:
-        print("está de tarde")
+        print("Está de tarde")
     else:
-        print("está de noite")
+        print("Está de noite")
 
